caecd9c9c44a69a938663496a07628b62df547e8
Sasha-OS <sashagrybyniuk123@gmail.com>
2021-09-05 20:08:12 +0300

    Add ability to  movement

--- lab1 - Space Invaders/main.py
+++ lab1 - Space Invaders/main.py
@@ -21,13 +21,31 @@ RED_PROJECTILE = pygame.image.load(os.path.join("assets", "red_projectile.png"))
 YELLOW_PROJECTILE = pygame.image.load(os.path.join("assets", "yellow_projectile.png"))
 GREEN_PROJECTILE = pygame.image.load(os.path.join("assets", "green_projectile.png"))
 
+# initializing enemy ship
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.projectile_img = None
+        self.projectiles = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
 
 def main():
     run = True
     level = 1
     lives = 3
+    kills = 0
+    player_speed = 5
     main_font = pygame.font.SysFont("comicsans", 40)
     FPS = 60
+
+    ship = Ship(300, 650)
+
     clock = pygame.time.Clock()
 
 
@@ -37,9 +55,13 @@ def main():
         #display info
         lives_label = main_font.render(f"Lives: {lives}", 1, (255, 5, 5))
         level_label = main_font.render(f"Level: {level}", 1, (255, 5, 5))
+        kills_label = main_font.render(f"Count: {kills}", 1, (255, 5, 5))
 
         WINDOW.blit(level_label, (10, 10))
+        WINDOW.blit(kills_label, (10, 50))
         WINDOW.blit(lives_label, (WIDTH - level_label.get_width() - 10, 10))
+        ship.draw(WINDOW)
+
         pygame.display.update()
 
     while run:
@@ -48,6 +70,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        #moving
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]: #left
+            ship.x -= player_speed
+        if keys[pygame.K_d]: #right
+            ship.x += player_speed
+        if keys[pygame.K_w]: #up
+            ship.y -= player_speed
+        if keys[pygame.K_s]: #down
+            ship.y += player_speed
 
 main()
